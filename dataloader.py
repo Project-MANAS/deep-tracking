0281cebc6e5fb1596ecc99ba822e536af37f561e
vdasu <vishnu98dasu@gmail.com>
2018-08-16 20:03:19 +0530

    Add __getitem__

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,7 @@ from torch.utils.serialization import load_lua
 import torch
 import numpy as np
 
+
 class DataLoader(object):
 
     def __init__(self, grid_minX, grid_maxX, grid_minY, grid_maxY, grid_step, sensor_start, sensor_step):
@@ -21,9 +22,12 @@ class DataLoader(object):
         self.sensor_start = -180
         self.sensor_step = 0.5
         
-    def __getitem__(self):
-        # TODO
-        pass
+    def __getitem__(self, i):
+        dist = self.data[i].__getitem__(self.index).reshape(self.height,self.width)
+        input = torch.empty((2, self.height, self.width),dtype=torch.float)
+        input[0] = torch.lt(torch.abs(dist - self.dist), self.grid_step * 0.7071)
+        input[1] = torch.gt(dist + self.grid_step * 0.7071, self.dist)
+        return input
     
     def getTrainingSize(self):
         return self.data.shape[0]
@@ -36,20 +40,22 @@ class DataLoader(object):
 
     def LoadSensorData(self, file):
         self.data = load_lua(file)
-        self.width = (self.grid_maxX - self.grid_minX) / self.grid_step + 1
-        self.height = (self.grid_maxY - self.grinf_minY) / self.grid_step + 1
-        self.dist = torch.empty((self.height, self.width),dtype=torch.float)
-        self.index = torch.empty((height,width),dtype=torch.long)
-        for y in self.height:
-            for x in self.width:
+        self.width = int((self.grid_maxX - self.grid_minX) / self.grid_step) + 1
+        self.height = int((self.grid_maxY - self.grid_minY) / self.grid_step) + 1
+        self.dist = torch.empty((self.height, self.width), dtype=torch.float)
+        self.index = torch.empty((self.height, self.width), dtype=torch.long)
+        for y in range(self.height):
+            for x in range(self.width):
                 px = x * self.grid_step + self.grid_minX
                 py = y * self.grid_step + self.grid_minY
-                angle = np.arctan2(px, py) * 180.0 / np.pi
+                angle = np.degrees(np.arctan2(px, py))
                 self.dist[y][x] = np.sqrt(px * px + py * py)
-                self.index[y][x] = np.floor((angle - self.sensor_start)/ self.sensor_step + 1.5)
+                self.index[y][x] = np.floor((angle - self.sensor_start)/ self.sensor_step + 1.5) - 1
         self.index = self.index.reshape(self.width * self.height)
+
 
     def test(self):
         print(self.sensor_start)
 
 obj = DataLoader()
+obj.LoadSensorData('/home/vdasu/Downloads/data.t7')

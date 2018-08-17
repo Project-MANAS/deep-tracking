e2e075c706efca798b64edf2fec49ef8a3c61c4d
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2018-08-17 17:25:09 +0530

    Refactor Code
    Rename functions to PEP8 naming standards
    grid_min* changed to min_*
    grid_max* changed to max_*
    Add default param values for __init__()
    Remove extra __init__()

--- dataloader.py
+++ dataloader.py
@@ -5,57 +5,50 @@ import numpy as np
 
 class DataLoader(object):
 
-    def __init__(self, grid_minX, grid_maxX, grid_minY, grid_maxY, grid_step, sensor_start, sensor_step):
-        self.grid_minX = grid_mixX
-        self.grid_maxX = grid_maxX
-        self.grid_minY = grid_minY
-        self.grid_maxY = grid_maxY
+    def __init__(self, x_bounds=(-25, 25), y_bounds=(-45, 5), grid_step=1, sensor_start=-180, sensor_step=0.5):
+        self.min_x, self.max_x = x_bounds
+        self.min_y, self.max_y = y_bounds
         self.grid_step = grid_step
-        self.sensor_start = sensor_step
+        self.sensor_start = sensor_start
+        self.sensor_step = sensor_step
 
-    def __init__(self):
-        self.grid_minX = -25
-        self.grid_maxX = 25
-        self.grid_minY = -45
-        self.grid_maxY = 5
-        self.grid_step = 1
-        self.sensor_start = -180
-        self.sensor_step = 0.5
-        
+        assert self.min_x < self.max_x
+        assert self.min_y < self.max_y
+
     def __getitem__(self, i):
-        dist = self.data[i].__getitem__(self.index).reshape(self.height,self.width)
-        input = torch.empty((2, self.height, self.width),dtype=torch.float)
-        input[0] = torch.lt(torch.abs(dist - self.dist), self.grid_step * 0.7071)
-        input[1] = torch.gt(dist + self.grid_step * 0.7071, self.dist)
-        return input
-    
-    def getTrainingSize(self):
+        dist = self.data[i].__getitem__(self.index).reshape(self.height, self.width)
+        input_data = torch.empty((2, self.height, self.width), dtype=torch.float)
+        input_data[0] = torch.lt(torch.abs(dist - self.dist), self.grid_step * 0.7071)
+        input_data[1] = torch.gt(dist + self.grid_step * 0.7071, self.dist)
+        return input_data
+
+    def get_training_size(self):
         return self.data.shape[0]
-    
-    def getHeight(self):
+
+    def get_height(self):
         return self.height
-    
-    def getWidth(self):
+
+    def get_width(self):
         return self.width
 
-    def LoadSensorData(self, file):
+    def load_sensor_data(self, file):
         self.data = load_lua(file)
-        self.width = int((self.grid_maxX - self.grid_minX) / self.grid_step) + 1
-        self.height = int((self.grid_maxY - self.grid_minY) / self.grid_step) + 1
+        self.width = int((self.max_x - self.min_x) / self.grid_step) + 1
+        self.height = int((self.max_y - self.min_y) / self.grid_step) + 1
         self.dist = torch.empty((self.height, self.width), dtype=torch.float)
         self.index = torch.empty((self.height, self.width), dtype=torch.long)
         for y in range(self.height):
             for x in range(self.width):
-                px = x * self.grid_step + self.grid_minX
-                py = y * self.grid_step + self.grid_minY
+                px = x * self.grid_step + self.min_x
+                py = y * self.grid_step + self.min_y
                 angle = np.degrees(np.arctan2(px, py))
                 self.dist[y][x] = np.sqrt(px * px + py * py)
                 self.index[y][x] = np.floor((angle - self.sensor_start)/ self.sensor_step + 1.5) - 1
         self.index = self.index.reshape(self.width * self.height)
 
-
     def test(self):
         print(self.sensor_start)
 
+
 obj = DataLoader()
-obj.LoadSensorData('/home/vdasu/Downloads/data.t7')
+obj.load_sensor_data('/home/vdasu/Downloads/data.t7')

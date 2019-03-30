503c3083bfec0aaf3a48c052cbccd7f40e04cb58
Chaitanya-git <chaitanya2.1999@gmail.com>
2019-03-31 00:07:25 +0530

    Refactor code to make the class more configurable

--- src/deep_tracker.py
+++ src/deep_tracker.py
@@ -9,19 +9,22 @@ from sensor_msgs.msg import CompressedImage
 from nav_msgs.msg import OccupancyGrid
 
 class DeepTracker:
-    def __init__(self):
+    def __init__(self, costmap_topic, visibility_layer_topic, publisher_topic, model_path, img_dim=(51,51)):
         batch_size = 1
-        self.img_dim = (51, 51)
+        self.img_dim = img_dim
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else cpu)
-        self.dt = torch.load('saved_models/model_8.pt') #DeepTrackerLSTM((3, batch_size, 16, img_dim, img_dim), False, False).to(device)
+        self.dt = torch.load(model_path) # or DeepTrackerLSTM((3, batch_size, 16, img_dim, img_dim), False, False).to(device)
         zero_tensor = torch.zeros((batch_size, 2, self.img_dim[0], self.img_dim[0])).to(self.device)
-        #self.dt = torch.jit.trace(self.dt, (zero_tensor))
         print(self.dt)
-        self.pub = rospy.Publisher('tracker_frames', CompressedImage, queue_size=10)
-        self.img_sub = message_filters.Subscriber('/move_base/local_costmap/obstacle_layer/obstacle_map', OccupancyGrid)
-        self.visibility_sub = message_filters.Subscriber('visibility_layer', CompressedImage)
+
+        self.pub = rospy.Publisher(publisher_topic, CompressedImage, queue_size=10)
+        
+        self.img_sub = message_filters.Subscriber(costmap_topic, OccupancyGrid)
+        self.visibility_sub = message_filters.Subscriber(visibility_layer_topic, CompressedImage)
+        
         ts = message_filters.ApproximateTimeSynchronizer([self.img_sub, self.visibility_sub], 10,10)
         ts.registerCallback(self.callback)
+        
         try:
             rospy.spin()
         except KeyboardInterrupt:
@@ -37,12 +40,15 @@ class DeepTracker:
         cv2.imshow('input', img)
         img = cv2.resize(img, self.img_dim)
         img = np.reshape(img, (1, 1, self.img_dim[0], self.img_dim[0]))
+
         visibility_layer = visibility_layer_msg.data
         visibility_layer = cv2.imdecode(np.fromstring(visibility_layer, np.uint8), cv2.IMREAD_GRAYSCALE)
         visibility_layer = np.resize(visibility_layer, self.img_dim)
         cv2.imshow('visibility', visibility_layer)
+        
         visibility_layer = np.reshape(visibility_layer, [1, 1, self.img_dim[0], self.img_dim[1]])
         img = np.concatenate([img, visibility_layer], axis=1)
+        
         with torch.no_grad():
             t_img = torch.tensor(img, dtype=torch.float).to(self.device)
             op = self.dt(t_img)
@@ -59,5 +65,9 @@ class DeepTracker:
 
 if __name__ == "__main__":
     rospy.init_node('deep_tracker')
-    DeepTracker()
+    publisher_topic = 'tracker_frames'
+    costmap_topic = '/move_base/local_costmap/obstacle_layer/obstacle_map'
+    visibility_layer_topic = 'visibility_layer'
+    model_path = 'saved_models/model_8.pt'
+    DeepTracker(costmap_topic, visibility_layer_topic, publisher_topic, model_path)
 
